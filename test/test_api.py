df3a1c1d9e6ea8b7d843f93fd529d6e2e9a4b048
oagutu <oobola@yahoo.com>
2018-03-23 15:23:18 +0300

    add unit test library setup

---
+++ test/test_api.py
@@ -0,0 +1,46 @@
+'''Testing of the various api endpoints'''
+
+import unittest
+
+class ApiEndpoint(unittest.TestCase):
+    '''Class holds the api endpoint test functions '''
+
+    def test_add_book(self):
+        '''Tests add_book() functionality'''
+        pass
+
+    def test_update_book(self):
+        '''Tests update_book() functionality'''
+        pass
+    
+    def test_remove_book(self):
+        '''Tests remove_book() functionality'''
+        pass
+
+    def test_retrieve_all_books(self):
+        '''Tests retrieve_all_books() functionality'''
+        pass
+
+    def test_get_book(self):
+        '''tests get_book() functionality'''
+        pass
+
+    def test_borrow_book(self):
+        '''tests borrow_book() functionality'''
+        pass
+
+    def test_create_user_account(self):
+        '''tests create_user_account() functionality'''
+        pass
+
+    def test_user_login(self):
+        '''tests user_login() functioanlity'''
+        pass
+
+    def test_user_logout(self):
+        '''tests user_logout() functionality'''
+        pass
+
+    def test_reset_password(self):
+        '''tests reset_password() functionality'''
+        pass
